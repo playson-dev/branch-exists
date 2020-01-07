9d3f9d2f6ff543ed3c07c87758d3656e77133e9c
Bjørn Bråthen <bjorbrat88@gmail.com>
2020-01-07 20:24:08 +0100

    test action

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,45 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
+import * as github from '@actions/github'
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`)
+    const repository: string = core.getInput('repository')
+    const [owner = null, repo = null] = repository.split('/')
+    const branch: string = core.getInput('branch')
+    const token: string = core.getInput('token') || ''
+    core.debug(`Checking branch ${owner}/${repo}#${branch}`)
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    if (!branch) {
+      throw new Error('Input "branch" must be set')
+    }
 
-    core.setOutput('time', new Date().toTimeString())
+    if (!repository) {
+      throw new Error('Input "repository" must be set')
+    }
+
+    if (owner === null || repo === null) {
+      throw new Error(
+        'Failed to parse input "repository". Must be on format: owner/repo'
+      )
+    }
+
+    try {
+      const gh = new github.GitHub(token)
+      const branchInfo = await gh.repos.getBranch({
+        repo,
+        branch,
+        owner
+      })
+      core.debug(`Found branch ${branchInfo}`)
+      core.setOutput('exists', 'true')
+      return
+    } catch (e) {
+      if (e.message === 'Branch not found') {
+        core.setOutput('exists', 'false')
+        return
+      }
+      throw new Error(`Failed to get branch: ${e.message}`)
+    }
   } catch (error) {
     core.setFailed(error.message)
   }
